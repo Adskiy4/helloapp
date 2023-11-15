febcc2e7074336ee73249e8bffe0d61339086429
Kirill <Vsadnikofhell@mail.ru>
2023-11-15 14:08:13 +0300

    Initial commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace HelloApp
+{
+    class Programm
+    {
+        public static void Main()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
