9ee4d667bbb6fe58190fd6029feb52972f828ead
Kirill <Vsadnikofhell@mail.ru>
2025-11-04 18:30:22 +0500

    task2 v5

--- HelloApp/run2.cs
+++ HelloApp/run2.cs
@@ -4,56 +4,112 @@ using System.Linq;
 
 class Program
 {
-    static List<string> Solve(Dictionary<string, List<string>> edges)
+    static List<string> Solve(Dictionary<string, List<string>> graph)
     {
-        var start = "a";
+        string virus = "a";
         var result = new List<string>();
-        var count = 0;
+
         while (true)
         {
-            var paths = BFS(edges, start);
-            if(paths.Count == 0)
-                break;
-            var path = paths
-                .OrderBy(p => p.Item1)
-                .ThenBy(p => p.Item2.Count > 1 ? p.Item2[1] : p.Item2[0])
+            var paths = BFS(graph, virus);
+            if (paths.Count == 0) break;
+
+            var cut = paths
+                .OrderBy(p => $"{p.gateway}-{p.prev}")
                 .First();
-            if (count != 0 && path.Item2.Count > 1)
-                start = path.Item2[1];
-            var concatNode = path.Item2[^1];
-            DeleteEdge(edges, concatNode, path.Item1);
-            result.Add($"{path.Item1}-{concatNode}");
-            count++;
+
+            DeleteEdge(graph, cut.gateway, cut.prev);
+            result.Add($"{cut.gateway}-{cut.prev}");
+
+            var pathsAfter = BFS(graph, virus);
+            if (pathsAfter.Count == 0) break;
+
+            var next = pathsAfter
+                .OrderBy(p => p.gateway)
+                .ThenBy(p => p.next)
+                .First();
+
+            virus = next.next;
         }
+
         return result;
     }
 
-    public static void DeleteEdge(Dictionary<string, List<string>> edges, string from, string to)
+    static void DeleteEdge(Dictionary<string, List<string>> edges, string from, string to)
     {
-        edges[from].Remove(to);
-        edges[to].Remove(from);
+        if (edges.ContainsKey(from)) edges[from].Remove(to);
+        if (edges.ContainsKey(to)) edges[to].Remove(from);
+    }
+
+    static List<(string gateway, string prev, string next)> BFS(Dictionary<string, List<string>> graph, string start)
+    {
+        var queue = new Queue<string>();
+        var parent = new Dictionary<string, string>();
+        var distance = new Dictionary<string, int>();
+        var gateways = new List<(string, string, string)>();
+        var minDistance = int.MaxValue;
+
+        queue.Enqueue(start);
+        distance[start] = 0;
+
+        while (queue.Count > 0)
+        {
+            var node = queue.Dequeue();
+            if (distance[node] > minDistance) break;
+
+            foreach (var neighbour in graph[node].OrderBy(v => v))
+            {
+                if (!distance.ContainsKey(neighbour))
+                {
+                    distance[neighbour] = distance[node] + 1;
+                    parent[neighbour] = node;
+                    if (char.IsUpper(neighbour[0]))
+                    {
+                        if (distance[neighbour] < minDistance)
+                        {
+                            minDistance = distance[neighbour];
+                            gateways.Clear();
+                        }
+                        if (distance[neighbour] == minDistance)
+                        {
+                            gateways.Add((neighbour, node, GetNextStep(parent, start, node)));
+                        }
+                    }
+                    else
+                    {
+                        queue.Enqueue(neighbour);
+                    }
+                }
+            }
+        }
+
+        return gateways;
+    }
+
+    public static string GetNextStep(Dictionary<string, string> parent, string start, string node)
+    {
+        var path = new Stack<string>();
+        while (node != start)
+        {
+            path.Push(node);
+            node = parent[node];
+        }
+        return path.Count > 0 ? path.Peek() : start;
     }
 
     static void Main()
     {
         var graph = new Dictionary<string, List<string>>();
         string line;
-
         while ((line = Console.ReadLine()) != null && line != "")
         {
-            line = line.Trim();
-            if (!string.IsNullOrEmpty(line))
+            var parts = line.Trim().Split('-');
+            if (parts.Length == 2)
             {
-                var parts = line.Split('-');
-                if (parts.Length == 2)
-                {
-                    if(!graph.ContainsKey(parts[0]))
-                        graph[parts[0]] = new List<string>();
-                    graph[parts[0]].Add(parts[1]);
-                    if(!graph.ContainsKey(parts[1]))
-                        graph[parts[1]] = new List<string>();
-                    graph[parts[1]].Add(parts[0]);
-                }
+                if (!graph.ContainsKey(parts[0])) graph[parts[0]] = new List<string>();
+                graph[parts[0]].Add(parts[1]);
+                if (!graph.ContainsKey(parts[1])) graph[parts[1]] = new List<string>();
+                graph[parts[1]].Add(parts[0]);
             }
         }
 
@@ -62,46 +118,5 @@ class Program
         {
             Console.WriteLine(edge);
         }
-    }
-
-    public static List<(string, List<string>)> BFS(Dictionary<string, List<string>> graph, string start)
-    {
-        var queue = new Queue<List<string>>();
-        var visited = new HashSet<string>();
-        queue.Enqueue(new List<string>(){start});
-        var paths = new List<(string, List<string>)>();
-        var minLength = int.MaxValue;
-        while (queue.Count > 0)
-        {
-            var path = queue.Dequeue();
-            var node = path[^1];
-            if ( path.Count > minLength)
-                continue;
-            visited.Add(node);
-            var neighbours = graph[node];
-            foreach (var neighbour in neighbours.OrderBy(v => v))
-            {
-                if(visited.Contains(neighbour))
-                    continue;
-                if (neighbour.Equals(neighbour.ToUpper()))
-                {
-                    if(path.Count == minLength)
-                        paths.Add((neighbour,path));
-                    else if (path.Count < minLength)
-                    {
-                        minLength = path.Count;
-                        paths = new List<(string, List<string>)>();
-                        paths.Add((neighbour, path));
-                    }
-                }
-                else
-                {
-                    var newPath = new List<string>(path) { neighbour };
-                    queue.Enqueue(newPath);
-                }
-            }
-        }
-
-        return paths;
     }
 }
